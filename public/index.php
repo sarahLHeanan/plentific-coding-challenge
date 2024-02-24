954197a874fa0fbff20125b3211f7eea56aef593
Sarah Heanan <sarahlheanan@gmail.com>
2024-02-24 23:38:12 +0000

    Demo getUser method
    class was also renamed

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Sarahheanan\PlentificCodingChallengeLibrary\RetrieveUsers;
+use Sarahheanan\PlentificCodingChallengeLibrary\User;
 ?>
 
 <!DOCTYPE html>
@@ -16,9 +16,18 @@ use Sarahheanan\PlentificCodingChallengeLibrary\RetrieveUsers;
 <body>
 
     <h1>Plentific Coding Challenge Implementation</h1>
+    <span><b>Status:</b></span>
     <?php
-        $users = new RetrieveUsers();
-        $users->testPackage();
+        $user = new User();
+        $user->testPackage() ?? 'package has not loaded';
+    ?>
+
+    <h2>Demo GetUser() method</h2>
+
+    <?php
+        // demo get user method
+        $user = $user->getUser(1);
+        var_dump($user);
     ?>
 
 </body>
